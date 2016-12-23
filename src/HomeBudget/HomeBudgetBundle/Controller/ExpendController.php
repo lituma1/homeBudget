ee64b82a9596f1d7da8b66272377b6c5e37c4eb7
Piotr Popkiewicz <lituma1@gmail.com>
2016-12-23 16:12:50 +0100

    Improve ExpendController

--- src/HomeBudget/HomeBudgetBundle/Controller/ExpendController.php
+++ src/HomeBudget/HomeBudgetBundle/Controller/ExpendController.php
@@ -70,17 +70,63 @@ class ExpendController extends Controller {
     }
 
     /**
-     * @Route("/expend/{id}/modify")
+     * @Route("/expend/{id}/modify", name="modify_expend")
      * @Security("has_role('ROLE_USER')")
      */
-    public function modifyExpendAction($id) {
-        return $this->render('HBBundle:Expend:modify_expend.html.twig', array(
-                        // ...
+    public function modifyExpendAction(Request $request, $id) {
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $message = '';
+
+        $repo = $this->getDoctrine()->getRepository('HBBundle:Expend');
+        $expend = $repo->findOneById($id);
+        $form = $this->createFormBuilder($expend)
+                ->add('description', 'text', array('label' => 'Opis wydatku'))
+                ->add('amount', 'number', array('label' => 'Kwota'))
+                ->add('expendDate', 'date', array('label' => 'Data'))
+                ->add('expendCategory', 'entity', array('class' => 'HBBundle:ExpendCategory',
+                    'query_builder' => function(EntityRepository $er) use ($user) {
+                        return $er->queryOwnedBy($user);
+                    },
+                    'choice_label' => 'name', 'label' => 'Kategoria'))
+                ->add('account', 'entity', array('class' => 'HBBundle:Account',
+                    'query_builder' => function(EntityRepository $er) use ($user) {
+                        return $er->queryOwnedBy($user);
+                    },
+                    'choice_label' => 'name', 'label' => 'Zapłacono z: '))
+                ->add('save', 'submit', array('label' => 'Potwierdź'))
+                ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+
+            $expend = $form->getData();
+
+
+            $expend->setUser($user);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($expend);
+            $account = $expend->getAccount();
+
+            $result = $account->spendMoney($expend->getAmount());
+
+
+
+
+
+            if ($result) {
+                $em->flush();
+                return $this->redirectToRoute('show_allExpends');
+            } else {
+                $message = 'Wydatek większy od salda, nie udało się go zapisać';
+            }
+        }
+        return $this->render('HBBundle:Expend:new_expend.html.twig', array(
+                    'form' => $form->createView(),
+                    'message' => $message
         ));
     }
 
     /**
-     * @Route("/expend/{id}/delete")
+     * @Route("/expend/{id}/delete", name="delete_expend")
      * @Security("has_role('ROLE_USER')")
      */
     public function deleteExpendAction($id) {
