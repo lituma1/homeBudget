7f5b6306449b06935a040a8c1f87bd09c18739db
Piotr Popkiewicz <lituma1@gmail.com>
2017-01-03 21:52:32 +0100

    Create first test for IncomeCategoryController

--- src/HomeBudget/HomeBudgetBundle/Tests/Controller/IncomeCategoryControllerTest.php
+++ src/HomeBudget/HomeBudgetBundle/Tests/Controller/IncomeCategoryControllerTest.php
@@ -6,17 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class IncomeCategoryControllerTest extends WebTestCase
 {
+    
+    private $client;
+
+    protected function setUp() {
+        $this->client = static::createClient(array(), array(
+                    'PHP_AUTH_USER' => 'Janek',
+                    'PHP_AUTH_PW' => '123123',
+        ));
+    }
+    
     public function testNewinccategory()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/incomeCategory/new');
-        
+        $crawler = $this->client->request('GET', '/incomeCategory/new', array(), array(), array(
+            'PHP_AUTH_USER' => 'Janek',
+            'PHP_AUTH_PW' => '123123',
+        ));
         $this->assertGreaterThan(
-            0,
-            $crawler->filter('html:contains("kategorię dla wydatku")')->count()
+                0, $crawler->filter('html:contains("Wprowadź nową kategorię dla przychodów")')->count()
         );
     }
+    
 
     public function testModifyinccategory()
     {
