2c80bdf7bda852d127da0942c4f5191ce712f733
Piotr Popkiewicz <lituma1@gmail.com>
2017-01-03 20:41:49 +0100

    First test for ExpendCategoryController

--- src/HomeBudget/HomeBudgetBundle/Tests/Controller/ExpendCategoryControllerTest.php
+++ src/HomeBudget/HomeBudgetBundle/Tests/Controller/ExpendCategoryControllerTest.php
@@ -6,11 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ExpendCategoryControllerTest extends WebTestCase
 {
+    
+    private $client;
+
+    protected function setUp() {
+        $this->client = static::createClient(array(), array(
+                    'PHP_AUTH_USER' => 'Janek',
+                    'PHP_AUTH_PW' => '123123',
+        ));
+    }
+    
     public function testNewexpcategory()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/expendCategory/new');
+        $crawler = $this->client->request('GET', '/expendCategory/new', array(), array(), array(
+            'PHP_AUTH_USER' => 'Janek',
+            'PHP_AUTH_PW' => '123123',
+        ));
+        $this->assertGreaterThan(
+                0, $crawler->filter('html:contains("Wprowadź nową kategorię dla wydatku")')->count()
+        );
     }
 
     public function testModifyexpcategory()
