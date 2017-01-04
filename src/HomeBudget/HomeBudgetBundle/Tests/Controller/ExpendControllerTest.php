afda2f0e5c46182ccf4b0cc78514349b943741ca
Piotr Popkiewicz <lituma1@gmail.com>
2017-01-04 16:04:12 +0100

    First tests for ExpendController

--- src/HomeBudget/HomeBudgetBundle/Tests/Controller/ExpendControllerTest.php
+++ src/HomeBudget/HomeBudgetBundle/Tests/Controller/ExpendControllerTest.php
@@ -6,18 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ExpendControllerTest extends WebTestCase
 {
+    
+    private $client;
+
+    protected function setUp() {
+        $this->client = static::createClient(array(), array(
+                    'PHP_AUTH_USER' => 'Janek',
+                    'PHP_AUTH_PW' => '123123',
+        ));
+    }
+    
     public function testNewexpend()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/expend/new');
+       $crawler = $this->client->request('GET', '/expend/new', array(), array(), array(
+            'PHP_AUTH_USER' => 'Janek',
+            'PHP_AUTH_PW' => '123123',
+        ));
+       $this->assertGreaterThan(
+                0, $crawler->filter('html:contains("Wprowadź nowy wydatek")')->count()
+        );
     }
 
     public function testModifyexpend()
     {
-        $client = static::createClient();
+        $crawler = $this->client->request('GET', '/expend/21/modify', array(), array(), array(
+            'PHP_AUTH_USER' => 'Janek',
+            'PHP_AUTH_PW' => '123123',
+        ));
 
-        $crawler = $client->request('GET', '/expend/{id}/modify');
+        $this->assertGreaterThan(
+                0, $crawler->filter('html:contains("Wprowadź modyfikację wydatku")')->count()
+        );
     }
 
     public function testDeleteexpend()
