c1842ccc7092777ae72820894d0d578d2f818d12
Piotr Popkiewicz <lituma1@gmail.com>
2017-01-07 11:48:00 +0100

    Create test for MainController

--- src/HomeBudget/HomeBudgetBundle/Tests/Controller/MainControllerTest.php
+++ src/HomeBudget/HomeBudgetBundle/Tests/Controller/MainControllerTest.php
@@ -4,13 +4,25 @@ namespace HomeBudget\HomeBudgetBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class MainControllerTest extends WebTestCase
-{
-    public function testShowmainpage()
-    {
-        $client = static::createClient();
+class MainControllerTest extends WebTestCase {
 
-        $crawler = $client->request('GET', '/Panel');
+    private $client;
+
+    protected function setUp() {
+        $this->client = static::createClient(array(), array(
+                    'PHP_AUTH_USER' => 'Janek',
+                    'PHP_AUTH_PW' => '123123',
+        ));
+    }
+
+    public function testShowmainpage() {
+        $crawler = $this->client->request('GET', '/panel', array(), array(), array(
+            'PHP_AUTH_USER' => 'Janek',
+            'PHP_AUTH_PW' => '123123',
+        ));
+        $this->assertGreaterThan(
+                0, $crawler->filter('html:contains("Aplikacja budżet domowy")')->count()
+        );
     }
 
 }
