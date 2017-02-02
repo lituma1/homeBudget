950f0cdb937016b361661b23cb7e3e5f947c9fe2
Piotr Popkiewicz <lituma1@gmail.com>
2017-02-02 17:35:15 +0100

    Create navbar with bootstrap

--- src/HomeBudget/HomeBudgetBundle/Controller/MainController.php
+++ src/HomeBudget/HomeBudgetBundle/Controller/MainController.php
@@ -13,26 +13,9 @@ class MainController extends Controller {
      * @Security("has_role('ROLE_USER')")
      */
     public function showMainPageAfterLoginAction() {
-
-        $links = [];
-        $linkAcc['href'] = 'show_allAccounts';
-        $linkAcc['text'] = "Konta";
-        $links[] = $linkAcc;
-        $linkExp['href'] = 'show_allExpends';
-        $linkExp['text'] = "Wydatki";
-        $links[] = $linkExp;
-        $linkInc['href'] = 'show_allIncomes';
-        $linkInc['text'] = "Przychody";
-        $links[] = $linkInc;
-        $linkEdit['href'] = 'fos_user_profile_edit';
-        $linkEdit['text'] = 'Edytuj swoje dane';
-        $links[] = $linkEdit;
-        $linkOut['href'] = 'fos_user_security_logout';
-        $linkOut['text'] = 'Wyloguj';
-        $links[] = $linkOut;
-
+       
         return $this->render('HBBundle:Main:show_main_page2.html.twig', array(
-                    'links' => $links
+                    
         ));
     }
 
@@ -46,17 +29,8 @@ class MainController extends Controller {
             return $this->redirectToRoute('Panel');
         }
 
-        $links = [];
-        $linkLog['href'] = 'fos_user_security_login';
-        $linkLog['text'] = "Logowanie";
-        $links[] = $linkLog;
-        $linkReg['href'] = 'fos_user_registration_register';
-        $linkReg['text'] = "Rejestracja";
-
-        $links[] = $linkReg;
-
         return $this->render('HBBundle:Main:show_main_page.html.twig', array(
-                    'links' => $links
+                    
         ));
     }
 
